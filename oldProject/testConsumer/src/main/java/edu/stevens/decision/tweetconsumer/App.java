7194406f73f2f3f5560419c6426db4191299d5a9
Michael Pereira <michaelpereira@jnickers12.mgnt.stevens-tech.edu>
2011-05-20 16:31:23 -0400

    auto exit after 1000 iterations

--- oldProject/testConsumer/src/main/java/edu/stevens/decision/tweetconsumer/App.java
+++ oldProject/testConsumer/src/main/java/edu/stevens/decision/tweetconsumer/App.java
@@ -7,6 +7,7 @@ import com.rabbitmq.client.QueueingConsumer;
 import edu.stevens.decision.tweetconsumer.hibernate.utils.HibernateUtil;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.hibernate.classic.Session;
 
 public class App
@@ -27,13 +28,20 @@ public class App
       System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
       QueueingConsumer consumer = new QueueingConsumer(chan);
       chan.basicConsume("twitterstream", true, consumer);
-      ExecutorService newCachedThreadPool = Executors.newFixedThreadPool(10);
-      while (true) {
+      ExecutorService newCachedThreadPool = Executors.newFixedThreadPool(5);
+      int i = 0;
+      while (i < 1000) {
         Session openSession = HibernateUtil.getSessionFactory().openSession();
         QueueingConsumer.Delivery delivery = consumer.nextDelivery();
         Worker newWorker = new Worker(delivery, openSession);
         newCachedThreadPool.execute(newWorker);
+        ++i;
+        System.out.println(i);
       }
+        System.out.println("done");
+      newCachedThreadPool.awaitTermination(5, TimeUnit.SECONDS);
+        System.out.println("waited");
+      Runtime.getRuntime().exit(1);
     } catch (OutOfMemoryError e) {
       throw e;
     } catch (org.hibernate.exception.ConstraintViolationException e)
