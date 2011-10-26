0c91efb2fe400b39bf366b4aceb50f9d9d749e1d
Michael Pereira <pereira.m@gmail.com>
2011-10-26 19:16:27 -0400

    new version of library, process 10,000 tweets

--- oldProject/testConsumer/src/main/java/edu/stevens/decision/tweetconsumer/App.java
+++ oldProject/testConsumer/src/main/java/edu/stevens/decision/tweetconsumer/App.java
@@ -12,47 +12,43 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import org.hibernate.classic.Session;
 
-public class App
-{
-
-  public static void main(String[] args)
-          throws java.io.IOException,
-          java.lang.InterruptedException,
-          ClassNotFoundException
-  {
-    try {
-        Properties properties = new Properties();
-        properties.load(new FileInputStream("src/main/resources/config.properties"));
-      Connection conn = null;
-      ConnectionFactory factory = new ConnectionFactory();
-      factory.setHost(properties.getProperty("RabbitMQHost"));
-      conn = factory.newConnection();
-      Channel chan = conn.createChannel();
-      chan.queueDeclare("twitterstream", false, false, false, null);
-      System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-      QueueingConsumer consumer = new QueueingConsumer(chan);
-      chan.basicConsume("twitterstream", true, consumer);
-      ExecutorService newCachedThreadPool = Executors.newFixedThreadPool(5);
-      int i = 0;
-      while (i < 1000) {
-        Session openSession = HibernateUtil.getSessionFactory().openSession();
-        Session openSession2 = HibernateUtil.getSessionFactory2().openSession();
-        QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-        Worker newWorker = new Worker(delivery, openSession, openSession2);
-        newCachedThreadPool.execute(newWorker);
-        ++i;
-        System.out.println(i);
-      }
-        System.out.println("done");
-      newCachedThreadPool.awaitTermination(5, TimeUnit.SECONDS);
-        System.out.println("waited");
-      Runtime.getRuntime().exit(1);
-    } catch (OutOfMemoryError e) {
-      throw e;
-    } catch (org.hibernate.exception.ConstraintViolationException e)
-    {
-      
-    } catch (org.hibernate.exception.GenericJDBCException e) {}
-    catch (org.hibernate.exception.DataException e) {}
-  }
+public class App {
+    
+    public static void main(String[] args)
+            throws java.io.IOException,
+            java.lang.InterruptedException,
+            ClassNotFoundException {
+        try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("src/main/resources/config.properties"));
+            Connection conn = null;
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost(properties.getProperty("RabbitMQHost"));
+            conn = factory.newConnection();
+            Channel chan = conn.createChannel();
+            chan.queueDeclare("twitterstream", false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+            QueueingConsumer consumer = new QueueingConsumer(chan);
+            chan.basicConsume("twitterstream", true, consumer);
+            ExecutorService newCachedThreadPool = Executors.newFixedThreadPool(5);
+            
+            
+            for (int i = 0; i < 10000; ++i) {
+                Session openSession = HibernateUtil.getSessionFactory().openSession();
+                Session openSession2 = HibernateUtil.getSessionFactory2().openSession();
+                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+                Worker newWorker = new Worker(delivery, openSession, openSession2);
+                newCachedThreadPool.execute(newWorker);
+            }
+            System.out.println("Processed 10000");
+            newCachedThreadPool.shutdown();          
+            Runtime.getRuntime().exit(0);
+        } catch (OutOfMemoryError e) {
+            throw e;
+        } catch (org.hibernate.exception.ConstraintViolationException e) {
+            
+        } catch (org.hibernate.exception.GenericJDBCException e) {
+        } catch (org.hibernate.exception.DataException e) {
+        }
+    }
 }
