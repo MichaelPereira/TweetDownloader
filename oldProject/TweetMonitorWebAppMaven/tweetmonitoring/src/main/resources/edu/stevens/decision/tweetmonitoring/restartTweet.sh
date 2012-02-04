ee8ecb1529cdc9d28a065e1b5b398675a95d16ab
Thomas Philippeau <tphilipp@stevens.edu>
2012-02-04 14:20:01 -0500

    Don't relaunch the program with the script

--- oldProject/TweetMonitorWebAppMaven/tweetmonitoring/src/main/resources/edu/stevens/decision/tweetmonitoring/restartTweet.sh
+++ oldProject/TweetMonitorWebAppMaven/tweetmonitoring/src/main/resources/edu/stevens/decision/tweetmonitoring/restartTweet.sh
@@ -1,4 +1,6 @@
 cd $1
 kill `cat toto.txt`
 rm toto.txt
-java -Xms$2 -Xmx$3 -jar $4
+
+# No need to relaunch the consumer because a loop is relaunching it automatically on the server
+# java -Xms$2 -Xmx$3 -jar $4
